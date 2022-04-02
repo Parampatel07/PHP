3847a8ca5850f508c844dd95d4b6f3b10837771d
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-04-02 20:52:12 +0530

    pdf library

---
+++ php/pdf_generator.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<!-- https://tcpdf.org/examples/example_001/ -->
+
+<body>
+    <div class="container mt-5">
+        <div class="row mt-5">
+            <div class="col-6 offset-3 mt-5 mb-5">
+                <div class="card mt-5 shadow">
+                    <div class="card-header bg-success text-light">
+                        <h2>PDF Generator</h2>
+                    </div>
+                    <div class="card-body">
+                        <form action="pdf_lib.php" method="POST" >
+                            <div class="mb-3">
+                                <label for="author_name" class="form-label">Enter Author Name</label>
+                                <input type="text" class="form-control" id="author_name" name="author_name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Set Title for Pdf</label>
+                                <input type="text" class="form-control" id="title" name="title">
+                            </div>
+                            <div class="mb-3">
+                                <label for="text" class="form-label">Enter Text in pdf file</label>
+                                <textarea class="form-control" name="text" placeholder="Leave a comment here"
+                                    id="text"></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label for="image">Select image file</label>
+                                <input type="file" name="image" id="image">
+                            </div>
+                            <div class="mb-3 text-end">
+                                <input type="submit" class="btn btn-success" value="Create Pdf">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ php/pdf_lib.php
@@ -0,0 +1,104 @@
+<?php
+require_once("../vendor/autoload.php");
+require_once('../vendor/tecnickcom/tcpdf/examples/tcpdf_include.php');
+// create new PDF document
+$author_name = $_POST['author_name'];
+$title = $_POST['title'];
+$text = $_POST['text'];
+$image = $_POST['image'];
+class MYPDF extends TCPDF {
+
+    //Page header
+    public function Header() {
+        // Logo
+        $image_file = K_PATH_IMAGES.'logo_example.jpg';
+        $this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+        // Set font
+        $this->SetFont('helvetica', 'B', 20);
+        // Title
+        $this->Cell(0, 15, '<< Pdf Generator >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+    }
+
+    // Page footer
+    public function Footer() {
+        // Position at 15 mm from bottom
+        $this->SetY(-15);
+        // Set font
+        $this->SetFont('helvetica', 'I', 8);
+        // Page number
+        $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+    }
+}
+
+
+
+
+// create new PDF document
+$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor($author_name);
+$pdf->SetTitle($title);
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+    require_once(dirname(__FILE__).'/lang/eng.php');
+    $pdf->setLanguageArray($l);
+}
+
+// ---------------------------------------------------------
+
+// set font
+$pdf->SetFont('times', 'BI', 12);
+
+// add a page
+$pdf->AddPage();
+
+// set some text to print
+$txt = <<<EOD
+$title
+$author_name
+
+$text
+<img src="<?php $image ?>" alt="">
+?>
+<?php
+EOD;
+
+
+
+// set JPEG quality
+$pdf->setJPEGQuality(75);
+// print a block of text using Write()
+$pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
+
+// ---------------------------------------------------------
+
+//Close and output PDF document
+$pdf->Output('example_003.pdf', 'I');
+
+?>
