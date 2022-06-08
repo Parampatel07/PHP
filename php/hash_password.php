a314bb821a8c784aa7962d2662524cc1af4f48d0
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-06-08 08:35:40 +0530

    j query

--- php/hash_password.php
+++ php/hash_password.php
@@ -1,15 +1,13 @@
 <?php
-    $param=986962;
+    $param=987987;
     $md5=md5($param);
     echo $md5;
     echo "<br>";
-
     $password_hash1=password_hash($param,PASSWORD_BCRYPT);
     $password_hash2=password_hash($param,PASSWORD_DEFAULT);
     echo $password_hash2;
     echo"<br>";
     echo $password_hash1;
-
     echo "<br>";
     $password=986962;
     $ismatch =password_verify($password,$password_hash1);
